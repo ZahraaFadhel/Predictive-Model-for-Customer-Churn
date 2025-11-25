7c5b812879936dfa317c51495c50a187cb9962d6
zfadhel <z.fadhel2004@gmail.com>
2025-11-25 15:47:06 +0300

    fix missing value + imputation justifications

--- get_dataset.py
+++ get_dataset.py
@@ -1,6 +1,17 @@
 import kagglehub
+import shutil
+import os
 
-# Download latest version of the dataset
-path = kagglehub.dataset_download("blastchar/telco-customer-churn", path="data/")
+# Download dataset
+path = kagglehub.dataset_download("blastchar/telco-customer-churn")
+print("Downloaded to:", path)
 
-print("Path to dataset files:", path)
+# Copy to project directory (current one)
+current_dir = os.getcwd()
+for file in os.listdir(path):
+    src = os.path.join(path, file)
+    if os.path.isfile(src):
+        shutil.copy2(src, current_dir)
+        print(f"Copied: {file}")
+
+print(f"\nDataset files are now in: {current_dir}")
